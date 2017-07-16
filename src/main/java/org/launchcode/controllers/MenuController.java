1e210ef9e5af019860de87911fe1990a948286cd
Alou Toure <alou_toure@yahoo.com>
2017-07-16 15:09:59 -0500

    Added MenuController(add) and add.html

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -1,12 +1,14 @@
 package org.launchcode.controllers;
 
 import com.sun.org.apache.xpath.internal.operations.Mod;
+import org.launchcode.models.Menu;
 import org.launchcode.models.data.CheeseDao;
 import org.launchcode.models.data.MenuDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.jws.WebParam;
 
@@ -28,5 +30,11 @@ public class MenuController {
         model.addAttribute("menus", menuDao.findAll());
 
         return "menu/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute(new Menu());
+        return "menu/add";
     }
 }
