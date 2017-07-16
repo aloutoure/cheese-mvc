3a403d954c4e12981dbf09405d18d5f54780f9fb
Alou Toure <alou_toure@yahoo.com>
2017-07-16 14:55:17 -0500

    Added MenuController and menu template

---
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -0,0 +1,32 @@
+package org.launchcode.controllers;
+
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import org.launchcode.models.data.CheeseDao;
+import org.launchcode.models.data.MenuDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.jws.WebParam;
+
+/**
+ * Created by Alou on 7/16/2017.
+ */
+
+@Controller
+@RequestMapping(value = "menu")
+public class MenuController {
+
+    @Autowired
+    MenuDao menuDao;
+
+    @Autowired
+    CheeseDao cheeseDao;
+
+    public String index(Model model) {
+        model.addAttribute("menus", menuDao.findAll());
+
+        return "menu/index";
+    }
+}
